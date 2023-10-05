2c1e9ec4e34ee1bd56131a14a379756dfde5adaf
OnePiece <961861383@qq.com>
2023-10-05 23:16:57 +0800

    webGis

---
+++ GisWeb/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './Components/Footer/Footer'
+import Main from './Components/Main/Main'
+function App() {
+  return (
+      <>
+      <Main/>
+      <Footer/>
+      </>
+  )
+}
+
+export default App
---
+++ GisWeb/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className="Footer">
+        <p>Made in XiaoChen &nbsp;<span>github:https://github.com/chenyinz</span></p>
+        
+    </div>
+  )
+}
+
+export default Footer
